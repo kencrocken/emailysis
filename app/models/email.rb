5c5f3ae94005e1eb6393fa709c054a56bf23e1f5
Ken Crocken <kcrocken@gmail.com>
2014-07-10 19:11:39 -0400

    maybe

--- app/models/email.rb
+++ app/models/email.rb
@@ -13,12 +13,12 @@ class Email < ActiveRecord::Base
         msg = imap.fetch(message_id,'RFC822')[0].attr['RFC822']
         mail = Mail.read_from_string msg
         mail_body = mail.multipart? ? mail.html_part : mail.body.decoded
-        #mail_body = Nokogiri::Slop(mail_body.to_s)
+        mail_body = Nokogiri::Slop(mail_body.to_s)
         email =  Email.create(from: mail.from.to_s, 
                               to: mail.to.to_s, 
                               sent_at: mail.date.to_time.strftime('%a %b %d %Y %H:%M:%S %Z'),
                               subject: mail.subject.to_s.force_encoding('UTF-8'),
-                              content: mail_body,
+                              content: mail_body.to_s,
                               folder: name,
                               project_id: project
                               )
