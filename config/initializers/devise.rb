9210912bbdd71d1a2081628166254384179f4125
Ken Crocken <kcrocken@gmail.com>
2014-07-11 00:23:34 -0400

    change up

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -230,7 +230,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   require 'omniauth-google-oauth2'
-  config.omniauth :google_oauth2, "1088870928026-6ngpfid4dpllh4chm78c58nsuvtu949h.apps.googleusercontent.com", "8zSlpOWS7JRo7XyIBL9qLbLn", 
+  config.omniauth :google_oauth2, "1088870928026-3evo71o30glos8nnq392vpjsfno1su86.apps.googleusercontent.com", "fG1bJGZy3oqijGAPWVNleJ_V", 
                                   { :scope => "https://mail.google.com/,email,profile,plus.me",
                                     :image_aspect_ratio => "square",
                                     :image_size => 100,
@@ -238,7 +238,7 @@ Devise.setup do |config|
                                     approval_prompt: "" }
 
 
- 
+# "1088870928026-6ngpfid4dpllh4chm78c58nsuvtu949h.apps.googleusercontent.com", "8zSlpOWS7JRo7XyIBL9qLbLn", 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
