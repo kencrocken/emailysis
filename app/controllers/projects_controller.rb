e7520f89e622b6981a08e7514de7a3b3cfbbccba
Ken Crocken <kcrocken@gmail.com>
2014-07-13 02:36:52 -0400

    heroku

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,35 +10,35 @@ class ProjectsController < ApplicationController
   def show
 
     @project = current_user.projects.find params[:id]
+    if !@project.emails.empty?
+      @inbox = @project.emails.order(sent_at: :desc).where folder: "inbox"
+      @sent = @project.emails.order(sent_at: :desc).where folder: "sent"
 
-    @inbox = @project.emails.order(sent_at: :desc).where folder: "inbox"
-    @sent = @project.emails.order(sent_at: :desc).where folder: "sent"
+      @inbox_date_array = []
+      @sent_date_array = []
+      @sender_array = []
+      @to_array = []
+      @start_date = @project.emails.first
+      @end_date = @project.emails.last
+      @start = @start_date[:sent_at]
+      @end = @end_date[:sent_at]
+      @date_difference = (@end.to_date - @start.to_date).to_i
+      @start_year = @start_date[:sent_at].to_time.strftime('%Y')
+      @start_month = @start_date[:sent_at].to_time.strftime('%m')
+      @start_month = @start_month.to_i
+      @start_month -= 1
+      @start_day = @start_date[:sent_at].to_time.strftime('%d')
 
-    @inbox_date_array = []
-    @sent_date_array = []
-    @sender_array = []
-    @to_array = []
-    @start_date = @project.emails.first
-    @end_date = @project.emails.last
-    @start = @start_date[:sent_at]
-    @end = @end_date[:sent_at]
-    @date_difference = (@end.to_date - @start.to_date).to_i
-    @start_year = @start_date[:sent_at].to_time.strftime('%Y')
-    @start_month = @start_date[:sent_at].to_time.strftime('%m')
-    @start_month = @start_month.to_i
-    @start_month -= 1
-    @start_day = @start_date[:sent_at].to_time.strftime('%d')
+      @inbox.each do |info|
 
-    @inbox.each do |info|
+        x = info.sent_at
+        x = x.to_time.to_i
+        x = x.to_s
+        @inbox_date_array << x
 
-      x = info.sent_at
-      x = x.to_time.to_i
-      x = x.to_s
-      @inbox_date_array << x
-
-      y = info.from
-      @sender_array << y
-
+        y = info.from
+        @sender_array << y
+      end
     end
 
     @sent.each do |info|
